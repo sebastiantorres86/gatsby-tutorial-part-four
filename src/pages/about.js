195d6b9af8dfebaa06ff57ae7d8e65a7725696c9
sebastiantorres86 <sebastian.torres.mail@gmail.com>
2020-09-24 13:27:30 -0300

    add SEO

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function About({ data }) {
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
       <h1>About {data.site.siteMetadata.title}</h1>
       <p>
         We're the only site running on your computer dedicated to showing the
