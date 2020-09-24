8c90946e0978056e64816cf8ae407fd78f8e66f8
sebastiantorres86 <sebastian.torres.mail@gmail.com>
2020-09-24 13:54:15 -0300

    fix again the SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import { css } from "@emotion/core"
 import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function Home({ data }) {
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1
           css={css`
